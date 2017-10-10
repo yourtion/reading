f8dee9755bf51b5fe194a31b9ec92411d28e96b0
YourtionGuo <yourtion@gmail.com>
2017-10-10 15:39:57 +0800

    update axios function

--- src/reading.js
+++ src/reading.js
@@ -29,19 +29,19 @@ app.post('/add', (req, res) => {
 
   if (action === 'opened') {
 
-    axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, {
-      headers: { 'Content-Type': 'application/json' },
-      data: { milestone: MILESTONE_ID },
-    }).then(() => console.info(`[END] set milestone successful! ${ html_url }`)).catch((e) => res.json(e));
+    axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, { milestone: MILESTONE_ID })
+      .then(() => console.info(`[END] set milestone successful! ${ html_url }`))
+      .catch((e) => res.json(e));
 
   } else if (action === 'milestoned') {
 
-    axios.put(`https://api.zenhub.io/p1/repositories/${ REPO_ID }/issues/${ number }/estimate?access_token=${ ZENHUB_ACCESS_TOKEN }`, {
-      headers: { 'Content-Type': 'application/json' },
-      data: { estimate: 1 },
-    }).then(() => console.info(`[END] Set estimate successful! ${ html_url }`)).catch((e) => console.error(`[END] Failed to set estimate! ${ html_url }`, e));
+    axios.put(`https://api.zenhub.io/p1/repositories/${ REPO_ID }/issues/${ number }/estimate?access_token=${ ZENHUB_ACCESS_TOKEN }`, { estimate: 1 })
+      .then(() => console.info(`[END] Set estimate successful! ${ html_url }`))
+      .catch((e) => console.error(`[END] Failed to set estimate! ${ html_url }`, e));
 
-    axios.post(`https://api.zenhub.io/v4/reports/release/${ RELEASE_ID }/items/issues?access_token=${ ZENHUB_ACCESS_TOKEN_V4 }`, {
+    axios({
+      method: 'post',
+      url: `https://api.zenhub.io/v4/reports/release/${ RELEASE_ID }/items/issues?access_token=${ ZENHUB_ACCESS_TOKEN_V4 }`,
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       data: `add_issues%5B0%5D%5Bissue_number%5D=${ number }&add_issues%5B0%5D%5Brepo_id%5D=${ REPO_ID }`,
     }).then(() => console.info(`[END] set release successful! ${ html_url }`)).catch((e) => console.error(`[END] Failed to set release! ${ html_url }`, e));
@@ -65,22 +65,19 @@ app.post('/close', (req, res) => {
       console.info('[RESULT]', data);
       if (data.total_count > 0) {
         data.items.forEach(({ url, html_url }) =>
-          axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, {
-            headers: { 'Content-Type': 'application/json' },
-            data: { state: 'closed' },
-          }).then(() => console.info(`[END] issue closed successful! ${ html_url }`)).catch(err => res.json('error', { error: err })));
+          axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, { state: 'closed' })
+            .then(() => console.info(`[END] issue closed successful! ${ html_url }`))
+            .catch(err => res.json('error', { error: err })));
         res.json({ message: 'Closed issue successful!' });
       } else {
-        axios.post(`https://api.github.com/repos/${ REPO_OWNER }/${ REPO_NAME }/issues?access_token=${ GITHUB_ACCESS_TOKEN }`, {
-          headers: { 'Content-Type': 'application/json' },
-          data: { title },
-        }).then(({ url, html_url }) => {
-          console.info(`[END] issue created successful! ${ html_url }`);
-          return axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, {
-            headers: { 'Content-Type': 'application/json' },
-            data: { state: 'closed' },
-          }).then(() => console.info(`[END] issue closed successful! ${ html_url }`)).catch(err => res.json('error', { error: err }));
-        })
+        axios.post(`https://api.github.com/repos/${ REPO_OWNER }/${ REPO_NAME }/issues?access_token=${ GITHUB_ACCESS_TOKEN }`, { title })
+          .then(({ url, html_url }) => {
+            console.info(`[END] issue created successful! ${ html_url }`);
+            return axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, {
+              headers: { 'Content-Type': 'application/json' },
+              data: { state: 'closed' },
+            }).then(() => console.info(`[END] issue closed successful! ${ html_url }`)).catch(err => res.json('error', { error: err }));
+          })
           .catch(err => res.json('error', { error: err }));
       }
       return res.json({ error: 'Finished achieve reading item!' });
