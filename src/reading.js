cebdde292ddac424033f920645c00f37a68c4013
YourtionGuo <yourtion@gmail.com>
2018-07-03 10:59:45 +0800

    update process for chinese keyword

--- src/reading.js
+++ src/reading.js
@@ -2,18 +2,17 @@
 const axios = require('axios');
 const express = require('express');
 const webtask = require('webtask-tools');
-const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const REPO_OWNER = 'yourtion';
 const REPO_NAME = 'reading';
 const REPO_ID = 105520376;
 
 const RELEASE_ID = '59dc65db0614f961b3e33e9d';
-const MILESTONE_ID = 1;
+const MILESTONE_ID = 2;
 
 app.get('/test', (req, res) => {
   res.end('Hello Yourtion');
@@ -29,7 +28,7 @@ app.post('/add', (req, res) => {
 
   if (action === 'opened') {
 
-    axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, { milestone: MILESTONE_ID })
+    return axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, { milestone: MILESTONE_ID })
       .then(() => console.info(`[END] set milestone successful! ${ html_url }`))
       .catch((e) => res.json(e));
 
@@ -44,7 +43,9 @@ app.post('/add', (req, res) => {
       url: `https://api.zenhub.io/v4/reports/release/${ RELEASE_ID }/items/issues?access_token=${ ZENHUB_ACCESS_TOKEN_V4 }`,
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       data: `add_issues%5B0%5D%5Bissue_number%5D=${ number }&add_issues%5B0%5D%5Brepo_id%5D=${ REPO_ID }`,
-    }).then(() => console.info(`[END] set release successful! ${ html_url }`)).catch((e) => console.error(`[END] Failed to set release! ${ html_url }`, e));
+    })
+    .then(() => console.info(`[END] set release successful! ${ html_url }`))
+    .catch((e) => console.error(`[END] Failed to set release! ${ html_url }`, e));
 
   }
 
@@ -57,32 +58,37 @@ app.post('/close', (req, res) => {
   console.info('[BEGIN]', req.query);
   const { title } = req.query;
 
-  const keyword = encodeURIComponent(title.replace(/\s/g, '+'));
+  const keyword = encodeURIComponent(title.replace(/[^\sa-zA-Z0-9_\u4e00-\u9fa5]/g, '+').replace(/\s/g, '+'));
   console.info('[KEYWORD]', keyword);
+  
+  if(!title || !keyword) return res.json({error: "no title"});
 
-  axios.get(`https://api.github.com/search/issues?q=${ keyword }%20repo:${ REPO_OWNER }/${ REPO_NAME }`)
+  return axios.get(`https://api.github.com/search/issues?q=${ keyword }%20repo:${ REPO_OWNER }/${ REPO_NAME }`)
     .then(({ data }) => {
       console.info('[RESULT]', data);
       if (data.total_count > 0) {
-        data.items.forEach(({ url, html_url }) =>
-          axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, { state: 'closed' })
-            .then(() => console.info(`[END] issue closed successful! ${ html_url }`))
-            .catch(err => res.json('error', { error: err })));
-        res.json({ message: 'Closed issue successful!' });
+        const { url, html_url } = data.items[0];
+        return axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, { state: 'closed', title: title })
+          .then(() => {
+            console.info(`[END] issue closed successful! ${ html_url }`)
+            return res.json({ message: 'Closed issue successful!' });
+          });
       } else {
-        axios.post(`https://api.github.com/repos/${ REPO_OWNER }/${ REPO_NAME }/issues?access_token=${ GITHUB_ACCESS_TOKEN }`, { title })
+        return axios.post(`https://api.github.com/repos/${ REPO_OWNER }/${ REPO_NAME }/issues?access_token=${ GITHUB_ACCESS_TOKEN }`, { title })
           .then(({ url, html_url }) => {
             console.info(`[END] issue created successful! ${ html_url }`);
             return axios.patch(`${ url }?access_token=${ GITHUB_ACCESS_TOKEN }`, {
               headers: { 'Content-Type': 'application/json' },
               data: { state: 'closed' },
-            }).then(() => console.info(`[END] issue closed successful! ${ html_url }`)).catch(err => res.json('error', { error: err }));
-          })
-          .catch(err => res.json('error', { error: err }));
+            }).then(() => {
+              console.info(`[END] issue closed successful! ${ html_url }`)
+              return res.json({ message: 'Closed issue successful!' });
+            })
+          });
       }
       return res.json({ error: 'Finished achieve reading item!' });
     })
-    .catch(err => res.json('error', { error: err }));
+    .catch(err => res.json({ error: err }));
 });
 
 module.exports = webtask.fromExpress(app);
